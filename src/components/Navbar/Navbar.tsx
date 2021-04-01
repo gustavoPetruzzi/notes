22a6986ce8db2a50880add2d422e43cf3e65c67d
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2021-04-01 14:20:38 -0300

    refactor navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,19 @@
 import { memo } from 'react';
+import { Bookmark } from '../Bookmark/Bookmark';
+import { BookmarkSize } from '../Bookmark/bookmark-size';
 import styles from './Navbar.module.scss';
 
 const NavbarComponent = () => {
   return (
-    <div className={styles.navbar}></div>
+    <div className={styles.navbar}>
+      <div className={styles.brand}>
+        <Bookmark size={BookmarkSize.SMALL}></Bookmark>
+        <p>Notes</p>
+      </div>
+      <div>
+        user
+      </div>
+    </div>
   )
 }
 
