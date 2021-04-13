8b82101cc91f61721ab0a6578608f0fee0ec864b
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2021-04-13 00:41:30 -0300

    add hook

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,20 +4,14 @@ import styles from './Modal.module.scss';
 interface Props {
   children: React.ReactNode,
   show: boolean
-  closeModal(): any
 }
 
 const ModalComponent = (props: Props) => {
-  const { children, show, closeModal } = props;
+  const { children, show } = props;
   const showModal = show ? styles.modal__show : null
   return (
     <div className={ `${styles.modal} ${showModal}` }>
       <div className={styles.modal__content}>
-        <div className={styles.modal__close}>
-          <div className={styles.modal__closeButton} onClick={closeModal}>
-            x
-          </div> 
-        </div>
         { children }
       </div>
     </div>
--- src/containers/Notes/Notes.tsx
+++ src/containers/Notes/Notes.tsx
@@ -6,6 +6,7 @@ import { Note } from '../../models/notes';
 import { getNotes } from '../../utils/notes-api';
 import styles from './Notes.module.scss'
 import { NoteForm } from '../../components/NotesForm/NotesForm';
+import useKeypress from '../../hooks/useKeypress';
 export const Notes = (props: { token: string }) => {
   const [notes, setNotes] = useState([{}]);
   const [isLoading, setIsloading] = useState(false);
@@ -23,6 +24,8 @@ export const Notes = (props: { token: string }) => {
 
   }, [props.token]);
 
+  useKeypress('Escape', () => setShowModal(false));
+
   const newNoteModal = () => {
     setShowModal(true);
   }
@@ -36,7 +39,7 @@ export const Notes = (props: { token: string }) => {
       {isLoading ? <h1>Loading...</h1> : null}
       {!isLoading  && notes.length > 0 ? <h1>NOTES</h1> : <h2> It seems that you don't have any notes :/</h2>}
       <Button type={ButtonType.DEFAULT} onClick={newNoteModal} >Add note</Button>
-      <Modal closeModal={closeModal} show={showModal}>
+      <Modal show={showModal}>
         <NoteForm onSave={() => console.log('print')}/>
       </Modal>
     </div>
