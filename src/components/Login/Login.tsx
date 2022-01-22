517367bdd23dd30b151f0a3dd27dddb40fa0efc5
Gustavo Petruzzi <gustavopetruzzi@gmail.com>
2022-01-22 14:35:17 -0300

    Implement new custom input in login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,26 @@
-import { Link } from 'react-router-dom';
-import styles from './Login.module.scss';
+import { Link } from "react-router-dom";
+import styles from "./Login.module.scss";
 import { useForm } from "react-hook-form";
-import { Bookmark } from '../Bookmark/Bookmark';
-import { BookmarkSize } from '../Bookmark/bookmark-size';
-import { ButtonType } from '../../models/button-type';
-import { getColor } from '../../utils/utils';
-import { LoginFormData } from '../../models/login-form-data';
+import { Bookmark } from "../Bookmark/Bookmark";
+import { BookmarkSize } from "../Bookmark/bookmark-size";
+import { ButtonType } from "../../models/button-type";
+import { getColor } from "../../utils/utils";
+import { LoginFormData } from "../../models/login-form-data";
+import Input from "../../ui/Input/Input";
 interface Props {
-  isLoading: boolean
-  onSave(formData: LoginFormData): void
+  isLoading: boolean;
+  onSave(formData: LoginFormData): void;
 }
 
 export const Login = (props: Props) => {
   const { isLoading, onSave } = props;
-  const { register, handleSubmit, formState: { errors }} = useForm<LoginFormData>({ mode: "onChange"});
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({ mode: "onChange" });
   const buttonType = getColor(ButtonType.Default);
-  const loadingIcon = isLoading ? 'is-loading' : '';
+  const loadingIcon = isLoading ? "is-loading" : "";
   const onSubmit = (data: LoginFormData) => onSave(data);
   return (
     <div className={styles.container}>
@@ -24,48 +29,40 @@ export const Login = (props: Props) => {
       </div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="field">
-          <label className="label"> Email </label>
-          <input 
-            name="email"
-            className="input"
-            placeholder="Email"
-            ref={register({
-              required: true,
-              pattern: /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/
-            })}
-          />
-          { 
-            errors?.email?.type === "required" && 
-            <p className="help is-danger">This is required</p>
-          }
-          {
-            errors?.email?.type === "pattern" &&
-            <p className="help is-danger">Invalid email</p>
-          }
-        </div>
-        <div className="field">
-          <label className="label"> Password</label>
-          <input
-            name="password"
-            className="input"
-            type="password"
-            placeholder="Password"
-            ref={register({
-              required: true
-            })}
-          />
-          { errors?.password?.type === "required" &&
-            <p className="help is-danger"> This is required</p>
-          }
-        </div>
+        <Input
+          type="email"
+          id="email"
+          label="email"
+          register={register({
+            required: true,
+            pattern: /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/,
+          })}
+        />
+        {errors?.email?.type === "required" && (
+          <p className="help is-danger">This is required</p>
+        )}
+        {errors?.email?.type === "pattern" && (
+          <p className="help is-danger">Invalid email</p>
+        )}
+
+        <Input
+          type="password"
+          id="password"
+          label="password"
+          register={register({
+            required: true,
+          })}
+        />
+        {errors?.password?.type === "required" && (
+          <p className="help is-danger"> This is required</p>
+        )}
         <button type="submit" className={`button ${buttonType} ${loadingIcon}`}>
           Login
         </button>
       </form>
       <Link to="/auth/signup">
-          Don't have an account? <strong>Signup</strong>
+        Don't have an account? <strong>Signup</strong>
       </Link>
     </div>
-  )
-}
+  );
+};
